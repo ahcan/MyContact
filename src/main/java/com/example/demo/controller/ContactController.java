5edcfa087bf64d3da3ceb4a38d41a4e834533e20
Huy Nguyen <tronghuy02@gmail.com>
2020-01-06 17:07:05 +0700

    define Controller

--- src/main/java/com/example/demo/controller/ContactController.java
+++ src/main/java/com/example/demo/controller/ContactController.java
@@ -2,10 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ContactController {
     @Autowired
     private ContactService contactService;
